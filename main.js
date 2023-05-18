60e3be9d9663f57859bfd3ce4114890d3ec5f04c
Joan D <joandidachr@gmail.comm>
2023-05-18 13:59:15 +0200

    Adds motion to navbar and Autocolor on X/Y axis

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three" ; 
 import './stles.css'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import gsap from "gsap";
 
 
 const scene = new THREE.Scene();
@@ -73,3 +74,35 @@ const loop = () => {
     window.requestAnimationFrame(loop)
 }
 loop()
+
+//Timeline Gsap
+
+const tl = gsap.timeline({defaults: {duration: 1 } })
+tl.fromTo(mesh.scale, { z:0, x:0, y:0 }, { z:1, x:1, y:1 })
+tl.fromTo('nav', {y:"-100%"}, { y:"0%"} )
+tl.fromTo('.title', { opacity: 0}, { opacity: 1 })
+
+//Mouse click Color change
+let mouseDown = false
+let rgb = []
+window.addEventListener('mousedown', () => (mouseDown = true))
+window.addEventListener('mouseup', () => (mouseDown = false))
+
+window.addEventListener('mousemove', (e) => {
+    if(mouseDown) {
+        rgb = [
+            Math.round((e.pageX / sizes.width) * 255),
+            Math.round((e.pageY / sizes.height) * 255),
+            150,
+        ]
+        // console.log(rgb)
+
+        //Animation mouse down colors
+        let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+        gsap.to(mesh.material.color, {
+            r:newColor.r,
+            g:newColor.g,
+            b:newColor.b      
+      })
+    }
+})
